0a7d442874f04026044de4c9e958cf99bbcdb9be
Emati Mitame <gitcommits@mita.me>
2017-04-08 21:38:58 +0100

    Fix db table name

--- followbot/db.py
+++ followbot/db.py
@@ -5,6 +5,6 @@ client = MongoClient(os.environ["MONGO_URL"])
 
 db = client["followbot"]
 
-user_table = db["dnf"]
-user_table.create_index("username", unique=True)
+user_table = db["users"]
+user_table.create_index("acct", unique=True)
 user_table.create_index("uid", unique=True)
