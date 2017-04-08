de14a7dcc5d5cca066369f1842076d02e8e30c96
Emati Mitame <gitcommits@mita.me>
2017-04-08 20:45:05 +0100

    Make bot and site executable

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python3
 from followbot.bot import App
 
 app = App()
-
 
 if __name__ == "__main__":
     app.run()
--- site.py
+++ site.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from followbot.site import app as application
 
 if __name__ == "__main__":
